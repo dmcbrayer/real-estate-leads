978b92c36cf89cd02a8372bc92599af9c8bd9dca
dmcbrayer <dmcbrayer@gmail.com>
2016-04-12 15:23:00 -0400

    Adds some error handling and custom rendering for home values.

--- gulp/assets/javascripts/components/SignUpForm/LastStep.jsx
+++ gulp/assets/javascripts/components/SignUpForm/LastStep.jsx
@@ -3,10 +3,21 @@ import React from 'react';
 export default class LastStep extends React.Component {
   constructor(props) {
     super(props);
+
+    this.renderValue = this.renderValue.bind(this)
   }
 
-  componentDidMount() {
+  componentWillMount() {
     this.props.getValue();
+  }
+
+  renderValue() {
+    if(this.props.value) {
+      return <p>This property's value is { this.props.value }</p>
+
+    } else {
+      return <p>No value is listed for this property!</p>
+    }
   }
 
   render() {
@@ -15,7 +26,8 @@ export default class LastStep extends React.Component {
         <h1>Success!</h1>
 
         <p>Your information will be sent to you shortly!</p>
-        <p>Value is {this.props.value}</p>
+        
+        { this.renderValue() }
       </div>
     )
   }
